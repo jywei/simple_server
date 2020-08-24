70b5db15ff37e0060c1eedcb77b0a59797c7b92a
Roy Wei <roywjy@gmail.com>
2020-08-24 16:41:08 +1000

    Add dependencies

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule SimpleServer.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :cowboy, :plug, :poison],
       mod: {SimpleServer.Application, []}
     ]
   end
@@ -24,6 +24,10 @@ defmodule SimpleServer.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:plug_cowboy, "~> 2.3"},
+      {:cowboy, "~> 2.8"},
+      {:plug, "~> 1.10"},
+      {:poison, "~> 3.1"}
     ]
   end
 end
